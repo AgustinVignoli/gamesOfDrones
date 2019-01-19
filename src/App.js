f37abb90bdc756f1fa907cdf52c49027a085d93f
Agustin Vignoli <avignoli@ascentis.com>
2019-01-19 20:47:45 -0300

    Adding styles to home screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
-    return <div className="App">{'Game of Drones'}</div>;
+    return (
+      <div className="title-screen">
+        <h1 className="game-title">Game of Drones</h1>
+        <Button size="large" variant="outlined" color="primary" href="/game" className="btn__basic">PLAY</Button>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,17 +3,39 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Particles from "react-particles-js";
 
+const params = {
+  particles: {
+    number: {
+      value: 1000
+    },
+    size: {
+      value: 3
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  }
+};
 const Game = () => <h1>Game Page</h1>;
 const Config = () => <h1>Config Page</h1>;
 
 ReactDOM.render(
-  <Router>
-    <Fragment>
-      <Route exact path="/" component={App} />
-      <Route path="/game" component={Game} />
-      <Route path="/config" component={Config} />
-    </Fragment>
-  </Router>,
+  <Fragment>
+    <Particles {...params} />
+    <Router>
+      <Fragment>
+        <Route exact path="/" component={App} />
+        <Route path="/game" component={Game} />
+        <Route path="/config" component={Config} />
+      </Fragment>
+    </Router>
+  </Fragment>,
   document.getElementById("root")
 );
