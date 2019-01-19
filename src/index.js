40bb3cadbeb381e8ea71ae84ca6edaa82e8260e4
Agustin Vignoli <avignoli@ascentis.com>
2019-01-19 19:47:42 -0300

    Adding more dependencies and basic routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Game = () => <h1>Game Page</h1>;
+const Config = () => <h1>Config Page</h1>;
+
+ReactDOM.render(
+  <Router>
+    <Fragment>
+      <Route exact path="/" component={App} />
+      <Route path="/game" component={Game} />
+      <Route path="/config" component={Config} />
+    </Fragment>
+  </Router>,
+  document.getElementById("root")
+);
